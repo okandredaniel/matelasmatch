8363abdbd9f7bc5e29f2a812f04523fe73ed41fe
okandredaniel <andre.fervilha@gmail.com>
2025-09-14 16:49:14 +0100

    chore: stop tracking tsbuildinfo

--- data/site.ts
+++
@@ -1,9 +0,0 @@
-import { SiteInfo } from "@/types/common"
-
-export const site: SiteInfo = {
-  name: "MatelasMatch",
-  url: "https://matelasmatch.fr",
-  email: "contact@matelasmatch.fr",
-  city: "Paris",
-  countryCode: "FR"
-}
--- types/testimonial.ts
+++
@@ -1,9 +0,0 @@
-export type Testimonial = {
-  id: string;
-  name: string;
-  initials: string;
-  location: string;
-  rating: number;
-  text: string;
-  gradient: string;
-};
